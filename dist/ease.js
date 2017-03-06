97f77d3848e9ad0075998bf5e8fd31204a14ab0b
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-03-06 20:51:44 +0200

    module: rebuild

--- dist/ease.js
+++ dist/ease.js
@@ -4,21 +4,8 @@ var ease = (function () {
   // Quarter circle
   var HALF_PI = Math.PI * 0.5;
 
-  // Turn things around, the out part
-  var flip = function flip(fn) {
-    return function (x) {
-      return 1 - fn(1 - x);
-    };
-  };
-
-  // Split down the middle, the in/out part
-  var fork = function fork(a, b) {
-    return function (x) {
-      return x < 0.5 ? a(2 * x) * 0.5 : 0.5 + b(2 * (x - 0.5)) * 0.5;
-    };
-  };
-
   // The more elaborate scaling formulas
+  // Stolen from: https://github.com/staltz/xstream/blob/master/src/extra/tween.ts
   var sine = function sine(x) {
     return 1 - Math.cos(x * HALF_PI);
   };
@@ -29,11 +16,26 @@ var ease = (function () {
     return -1 * (Math.sqrt(1 - x * x) - 1);
   };
 
+  // Invert, the out part
+  var flip = function flip(fn) {
+    return function (x) {
+      return 1 - fn(1 - x);
+    };
+  };
+
+  // Split in half, the in/out part
+  var fork = function fork(a, b) {
+    return function (x) {
+      return x < 0.5 ? a(2 * x) * 0.5 : 0.5 + b(2 * (x - 0.5)) * 0.5;
+    };
+  };
+
   // Drive out, in/out from in, band together
   var ease = function ease(fn) {
     var fnOut = flip(fn);
     var fnInOut = fork(fn, fnOut);
 
+    // Feed the slope
     return {
       in: function _in(t, d) {
         return fn(t / d);
--- index.js
+++ index.js
@@ -3,21 +3,8 @@
 // Quarter circle
 var HALF_PI = Math.PI * 0.5;
 
-// Turn things around, the out part
-var flip = function flip(fn) {
-  return function (x) {
-    return 1 - fn(1 - x);
-  };
-};
-
-// Split down the middle, the in/out part
-var fork = function fork(a, b) {
-  return function (x) {
-    return x < 0.5 ? a(2 * x) * 0.5 : 0.5 + b(2 * (x - 0.5)) * 0.5;
-  };
-};
-
 // The more elaborate scaling formulas
+// Stolen from: https://github.com/staltz/xstream/blob/master/src/extra/tween.ts
 var sine = function sine(x) {
   return 1 - Math.cos(x * HALF_PI);
 };
@@ -28,11 +15,26 @@ var circ = function circ(x) {
   return -1 * (Math.sqrt(1 - x * x) - 1);
 };
 
+// Invert, the out part
+var flip = function flip(fn) {
+  return function (x) {
+    return 1 - fn(1 - x);
+  };
+};
+
+// Split in half, the in/out part
+var fork = function fork(a, b) {
+  return function (x) {
+    return x < 0.5 ? a(2 * x) * 0.5 : 0.5 + b(2 * (x - 0.5)) * 0.5;
+  };
+};
+
 // Drive out, in/out from in, band together
 var ease = function ease(fn) {
   var fnOut = flip(fn);
   var fnInOut = fork(fn, fnOut);
 
+  // Feed the slope
   return {
     in: function _in(t, d) {
       return fn(t / d);
