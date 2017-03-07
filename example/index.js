df911c6ff32a806d77e8a3efbeb636e6d48c9612
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-03-07 14:56:40 +0200

    example: add control point approximations for css timing fn

--- example/index.js
+++ example/index.js
@@ -17,19 +17,20 @@ var plot = function plot(buffer, easing) {
 
     buffer.beginPath();
     buffer.arc(x, y, 1, 0, TAU);
-    buffer.fillStyle = '#fff';
+    buffer.fillStyle = '#000';
     buffer.fill();
 
     buffer.restore();
   }
 };
 
-var list = document.getElementById('figure');
-var adam = list.removeChild(list.querySelector('figure'));
+var list = document.querySelector('ul');
+var adam = list.removeChild(list.querySelector('li'));
+var data = getData();
 
-var plots = [];
 var types = 'sine,quad,cubic,quart,circ,expo'.split(',');
 var paths = 'in,out,inOut'.split(',');
+var plots = [];
 
 var totalPaths = paths.length;
 var totalTypes = types.length;
@@ -42,11 +43,56 @@ for (var i = 0; i < total; i += 1) {
   var papa = adam.cloneNode(true);
   var turf = papa.firstChild.getContext('2d');
 
-  plot(turf, ease[type][path]);
+  var points = data[type][path].join(', ');
+  var easing = ease[type][path];
 
-  papa.setAttribute('data-ease', type + '/' + path);
-  papa.setAttribute('class', type + '-' + path);
+  papa.setAttribute('data-ease', type + '.' + path);
+  papa.setAttribute('style', `transition-timing-function: cubic-bezier(${points});`);
 
   list.appendChild(papa);
+
+  plot(turf, easing);
 }
 
+// Cubic bezier control point approximations for Penner's equations
+// From: https://github.com/zz85/cubic-bezier-approximations
+// Also: https://github.com/KinkumaDesign/CustomMediaTimingFunction
+function getData() {
+ return {
+   quad: {
+     in: [0.26, 0, 0.6, 0.2],
+     out: [0.4, 0.8, 0.74, 1],
+     inOut: [0.48, 0.04, 0.52, 0.96],
+   },
+   cubic: {
+     in: [0.4, 0, 0.68, 0.06],
+     out: [0.32, 0.94, 0.6, 1],
+     inOut: [0.66, 0, 0.34, 1],
+   },
+   quart: {
+     in: [0.52, 0, 0.74, 0],
+     out: [0.26, 1, 0.48, 1],
+     inOut: [0.76, 0, 0.24, 1],
+   },
+   quint: {
+     in: [0.64, 0, 0.78, 0],
+     out: [0.22, 1, 0.36, 1],
+     inOut: [0.84, 0, 0.16, 1],
+   },
+   sine: {
+     in: [0.32, 0, 0.6, 0.36],
+     out: [0.4, 0.64, 0.68, 1],
+     inOut: [0.36, 0, 0.64, 1],
+   },
+   expo: {
+     in: [0.66, 0, 0.86, 0],
+     out: [0.14, 1, 0.34, 1],
+     inOut: [0.9, 0, 0.1, 1],
+   },
+   circ: {
+     in: [0.54, 0, 1, 0.44],
+     out: [0, 0.56, 0.46, 1],
+     inOut: [0.88, 0.14, 0.12, 0.86],
+   },
+ };
+}
