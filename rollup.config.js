a488687d05f6b3c94e909645ca48492194f40c0f
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-06-22 08:11:52 +0200

    package: cleanup rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,21 +3,17 @@ import babel from 'rollup-plugin-babel';
 export default {
   entry: 'index.es',
   plugins: [
-    babel({
-      exclude: 'node_modules/**'
-    }),
+    babel()
   ],
   targets: [
     {
       format: 'iife',
-      indent: true,
-      sourceMap: true,
       moduleName: 'ease',
-      dest: 'dist/ease.js'
+      dest: 'dist/ease.js',
     },
     {
       format: 'cjs',
-      dest: 'index.js'
+      dest: 'index.js',
     }
   ]
 };
