e01292bebe0433a424fca9eb8fc0b29b3df22c26
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-01-04 16:49:24 +0100

    module: rebuild cjs version

--- index.js
+++ index.js
@@ -6,41 +6,41 @@ Object.defineProperty(exports, '__esModule', { value: true });
 // Helps with the tweens
 
 // Quarter circle
-const HALF_PI = Math.PI * 0.5;
+var HALF_PI = Math.PI * 0.5;
 
 // Invert, the out part
-const flip = fn => x => 1 - fn(1 - x);
+var flip = function (fn) { return function (x) { return 1 - fn(1 - x); }; };
 
 // Split in half, the in/out part
-const fork = (a, b) => x => ((x < 0.5) ? (a(2 * x) * 0.5) : (0.5 + (b(2 * (x - 0.5)) * 0.5)));
+var fork = function (a, b) { return function (x) { return ((x < 0.5) ? (a(2 * x) * 0.5) : (0.5 + (b(2 * (x - 0.5)) * 0.5))); }; };
 
 // Drive out, in/out from in, band together
 // https://github.com/staltz/xstream/blob/master/src/extra/tween.ts
-const from = (fn) => {
-  const fnOut = flip(fn);
-  const fnInOut = fork(fn, fnOut);
+var from = function (fn) {
+  var fnOut = flip(fn);
+  var fnInOut = fork(fn, fnOut);
 
   // Feed each with slope
   // (current position over duration)
   return {
-    in: (t, d) => fn(t / d),
-    out: (t, d) => fnOut(t / d),
-    inOut: (t, d) => fnInOut(t / d)
+    in: function (t, d) { return fn(t / d); },
+    out: function (t, d) { return fnOut(t / d); },
+    inOut: function (t, d) { return fnInOut(t / d); }
   }
 };
 
 // The basic exponential definitions
-const quad = from(x => x ** 2);
-const cubic = from(x => x ** 3);
-const quart = from(x => x ** 4);
-const quint = from(x => x ** 5);
-const expo = from(x => 2 ** (10 * (x - 1)));
+var quad = from(function (x) { return Math.pow( x, 2 ); });
+var cubic = from(function (x) { return Math.pow( x, 3 ); });
+var quart = from(function (x) { return Math.pow( x, 4 ); });
+var quint = from(function (x) { return Math.pow( x, 5 ); });
+var expo = from(function (x) { return Math.pow( 2, (10 * (x - 1)) ); });
 
 // The smooth
-const sine = from(x => 1 - Math.cos(x * HALF_PI));
+var sine = from(function (x) { return 1 - Math.cos(x * HALF_PI); });
 
 // The snaky
-const circ = from(x => -1 * (Math.sqrt(1 - (x * x)) - 1));
+var circ = from(function (x) { return -1 * (Math.sqrt(1 - (x * x)) - 1); });
 
 exports.quad = quad;
 exports.cubic = cubic;
@@ -49,3 +49,4 @@ exports.quint = quint;
 exports.expo = expo;
 exports.sine = sine;
 exports.circ = circ;
+
